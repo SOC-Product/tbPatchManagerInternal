13d6a23c7e91acf522ba9433834677a80f254d18
Saurabh Singh <saurabh.singh@tech-bridge.biz>
2026-02-05 16:38:41 +0530

    Added maintainance group schema and mapping with asset

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,5 +1,14 @@
 export const CONSTANT = {
     AD_SYNC_BATCH_SIZE: 100,
 
-    AD_SYNC_COLUMNS: ['computer_name', 'distinguished_name', 'operating_system', 'operating_system_version', 'dns_host_name', 'created_at', 'updated_at', 'source']
+    AD_SYNC_COLUMNS: [
+        'computer_name', 
+        'distinguished_name', 
+        'operating_system', 
+        'operating_system_version', 
+        'dns_host_name', 
+        'created_at', 
+        'updated_at', 
+        'source'
+    ]
 }
