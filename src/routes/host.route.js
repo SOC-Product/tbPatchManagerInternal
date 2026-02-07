b635e552d2950c37bcd293e9a8b9fb8c02086296
tbgautam <gautam.kumar@tech-bridge.biz>
2026-02-07 13:53:04 +0530

    adding checks

--- src/routes/host.route.js
+++ src/routes/host.route.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import hostController from '../controllers/host.controller.js';
-import { uploadSSHKey } from '../middleware/upload.middleware.js';
+import express from "express";
+import hostController from "../controllers/host.controller.js";
+import { uploadSSHKey } from "../middleware/upload.middleware.js";
 
 const router = express.Router();
 
 // Host CRUD operations
 router
-  .route('/')
+  .route("/")
   .get(hostController.getAllHosts)
-  .post(uploadSSHKey.single('ssh_key'), hostController.createAdHost);
-  
+  .post(uploadSSHKey.single("ssh_key"), hostController.createAdHost);
 
+// just check
 export default router;
