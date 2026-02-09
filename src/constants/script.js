003375e37a3e8e442a78100901bf6f59a11b2959
Saurabh Singh <saurabh.singh@tech-bridge.biz>
2026-02-09 11:28:07 +0530

    Added get kpi data route for group

--- src/constants/script.js
+++ src/constants/script.js
@@ -88,17 +88,37 @@ export const SCRIPT = {
       m.risk_tolerance,
       m.description,
       COALESCE(ARRAY_AGG(mghm.host_id), '{}') AS assets
-    FROM maintainance_group m
-    LEFT JOIN maintainance_group_host_mapping mghm 
-      ON mghm.maintainance_group_id = m.id
-    WHERE m.id = $1
-    GROUP BY m.id;
+      FROM maintainance_group m
+      LEFT JOIN maintainance_group_host_mapping mghm 
+            ON mghm.maintainance_group_id = m.id
+        WHERE m.id = $1
+        GROUP BY m.id;
+        `,
+  DELETE_GROUP_ASSET_MAPPING: 
+    ` DELETE FROM maintainance_group_host_mapping 
+      WHERE maintainance_group_id = $1`,
+  DELETE_GROUP: `DELETE FROM maintainance_group WHERE id = $1`,
+  
+  UPDATE_GROUP: `
+    UPDATE maintainance_group 
+    SET name = $2, risk_tolerance = $3, description = $4, updated_at = NOW() 
+    WHERE id = $1
   `,
 
-  DELETE_GROUP_ASSET_MAPPING: `
-    DELETE FROM maintainance_group_host_mapping 
-    WHERE maintainance_group_id = $1
-  `,
+  GET_GROUP_ASSETS: `SELECT host_id FROM maintainance_group_host_mapping WHERE maintainance_group_id = $1`,
+  DELETE_ASSETS: `
+        DELETE FROM maintainance_group_host_mapping 
+        WHERE 
+        maintainance_group_id = $1 
+        AND host_id = ANY($2);`,
+  
+  MAINTAINANCE_GROUP_KPI: 
+      ` SELECT count(*), 
+        SUM(CASE WHEN risk_tolerance='high' THEN 1 ELSE 0 END) AS CRITICAL 
+        from maintainance_group;`,
+  GET_ASSETS_IN_GROUP: 
+      ` SELECT count(*) 
+        from maintainance_group_host_mapping`,
 
   DELETE_GROUP: `
     DELETE FROM maintainance_group 
