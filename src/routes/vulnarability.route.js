c393f1348b920ce5851e1cd470610beb6f0f4f34
Saurabh Singh <saurabh.singh@tech-bridge.biz>
2026-02-12 17:13:52 +0530

    Fixed upload csv middleware with .csv extension

--- src/routes/vulnarability.route.js
+++ src/routes/vulnarability.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import vulnerabilityController from '../controllers/vulnerability.controller.js';
-import { uploadCsvFile } from '../middleware/upload.middleware.js';
+import { uploadCsvFileMiddleware } from '../middleware/upload.middleware.js';
 
 const router = express.Router();
 
 router
-  .post('/parseCsvVulnerability', uploadCsvFile.single('file') ,vulnerabilityController.parseCsvVulnerability)
+  .post('/parseCsvVulnerability', uploadCsvFileMiddleware ,vulnerabilityController.parseCsvVulnerability)
   .post('/addVulnerability', vulnerabilityController.addLinuxVulnerability)
   .post('/addWindowsVulnerability', vulnerabilityController.addWindowsVulnerability)
 
