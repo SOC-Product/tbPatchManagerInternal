db7ef1fc3cf4732b48222f20721f58a72d64fd9d
Saurabh Singh <saurabh.singh@tech-bridge.biz>
2026-02-13 14:23:02 +0530

    WIP: download patch api

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import hostRouter from "./host.route.js";
 import groupRouter from "./group.route.js";
 import vulnerabilityRouter from "./vulnarability.route.js"
+import patchRouter from "./patch.route.js"
 const router = express.Router();
 
 // Mount host routes
 router.use("/hosts", hostRouter);
 router.use("/group", groupRouter);
 router.use("/vulnerability", vulnerabilityRouter);
+router.use("/patch", patchRouter);
 
 export default router;
